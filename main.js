06840110449a5d5958da8da1a2583628d76e9212
Skatehead <clubhread@gmail.com>
2022-11-11 08:57:43 +0200

    12

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 const button = document.querySelector(".btn");
 let stonesIndex = document.querySelectorAll(".stone");
+const stoneTransition = document.querySelector(".gameField");
 const stonesArray = [];
 let nodeArray = [];
 let stoneInTransition = 100;
 let direction;
-const stoneTransition = document.querySelector(".gameField");
 
 function shuffleStones() {
   const unshuffled = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
-
+  const stonesIndex = document.querySelectorAll(".stone");
   let shuffled = unshuffled
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
 
   for (let i = 0; i < 16; i++) {
-    stonesArray[i] = shuffled[i];
+    //stonesArray[i] = shuffled[i];
     stonesIndex[i].style.backgroundImage = "none";
     if (shuffled[i] !== 0) {
       stonesIndex[i].style.backgroundImage = `url(stone_${shuffled[i]}.png)`;
     }
+
+    const atr = document.createAttribute("data-nom");
+    atr.value = `${shuffled[i]}`;
+    stonesIndex[i].setAttributeNode(atr);
   }
 }
 
@@ -31,38 +35,31 @@ function swapNodes(node1, node2) {
 }
 
 // Checks if there is a blank stone (item = 0) around stone chosen and put the stone there
-function processStones(evt) {
-  if (stoneInTransition !== 100) {
-    return;
-  }
-
+function processStones(event) {
+  const stonesIndex = document.querySelectorAll(".stone");
   // when this function is used as an event handler: this === evt.currentTarget
   nodeArray = Array.from(stonesIndex);
-  const stoneIndex = nodeArray.indexOf(this); // index of the stone chosen
-  const stoneItem = stonesArray[stoneIndex]; // item of the stone chosen
-  stoneInTransition = stoneIndex;
+
+  /* for (const currentNode of nodeArray) {
+    if (currentNode.style.transform !== "none") {
+      return;
+    }
+  }*/
+
+  const stoneIndex = nodeArray.indexOf(event.target); // index of the stone chosen
 
   if (stoneIndex > 3) {
     // check blank stone above the stone chosen
-    if (stonesArray[stoneIndex - 4] === 0) {
-      stonesArray[stoneIndex - 4] = stoneItem;
-      stonesArray[stoneIndex] = 0;
-
-      //refreshBoard();
-      this.style.transform = "translateY(-150px)";
-      direction = "up";
+    if (nodeArray[stoneIndex - 4].dataset.nom === "0") {
+      event.target.style.transform = "translateY(-100%)";
       return;
     }
   }
 
   if (stoneIndex < 12) {
     // check blank stone beneath the stone chosen
-    if (stonesArray[stoneIndex + 4] === 0) {
-      stonesArray[stoneIndex + 4] = stoneItem;
-      stonesArray[stoneIndex] = 0;
-      //refreshBoard();
-      this.style.transform = "translateY(150px)";
-      direction = "down";
+    if (nodeArray[stoneIndex + 4].dataset.nom === "0") {
+      event.target.style.transform = "translateY(100%)";
       return;
     }
   }
@@ -74,12 +71,8 @@ function processStones(evt) {
     stoneIndex !== 11 &&
     stoneIndex !== 15
   ) {
-    if (stonesArray[stoneIndex + 1] === 0) {
-      stonesArray[stoneIndex + 1] = stoneItem;
-      stonesArray[stoneIndex] = 0;
-      //refreshBoard();
-      this.style.transform = "translateX(150px)";
-      direction = "right";
+    if (nodeArray[stoneIndex + 1].dataset.nom === "0") {
+      event.target.style.transform = "translateX(100%)";
       return;
     }
   }
@@ -91,16 +84,11 @@ function processStones(evt) {
     stoneIndex !== 8 &&
     stoneIndex !== 12
   ) {
-    if (stonesArray[stoneIndex - 1] === 0) {
-      stonesArray[stoneIndex - 1] = stoneItem;
-      stonesArray[stoneIndex] = 0;
-      //refreshBoard();
-      this.style.transform = "translateX(-150px)";
-      direction = "left";
+    if (nodeArray[stoneIndex - 1].dataset.nom === "0") {
+      event.target.style.transform = "translateX(-100%)";
       return;
     }
   }
-  stoneInTransition = 100;
 }
 
 // Add click listener to the button for restart
@@ -108,12 +96,39 @@ button.addEventListener("click", shuffleStones);
 
 // Add a click listener to each stone
 
-for (const index of stonesIndex) {
-  index.addEventListener("click", processStones);
-}
+stoneTransition.addEventListener("click", processStones);
 
-stoneTransition.addEventListener("transitionend", () => {
-  switch (direction) {
+stoneTransition.addEventListener("transitionend", (event) => {
+  const stonesIndex = document.querySelectorAll(".stone");
+  const nodeArray = Array.from(stonesIndex);
+
+  if (event.target.style.transform === "translateY(-100%)") {
+    swapNodes(
+      nodeArray[nodeArray.indexOf(event.target)],
+      nodeArray[nodeArray.indexOf(event.target) - 4]
+    );
+  } else if (event.target.style.transform === "translateY(100%)") {
+    swapNodes(
+      nodeArray[nodeArray.indexOf(event.target)],
+      nodeArray[nodeArray.indexOf(event.target) + 4]
+    );
+  } else if (event.target.style.transform === "translateX(100%)") {
+    swapNodes(
+      nodeArray[nodeArray.indexOf(event.target)],
+      nodeArray[nodeArray.indexOf(event.target) + 1]
+    );
+  } else if (event.target.style.transform === "translateX(-100%)") {
+    swapNodes(
+      nodeArray[nodeArray.indexOf(event.target)],
+      nodeArray[nodeArray.indexOf(event.target) - 1]
+    );
+  }
+
+  event.target.style.transform = "none";
+  return;
+});
+
+/*  switch (direction) {
     case "up":
       swapNodes(
         stonesIndex[stoneInTransition],
@@ -143,7 +158,7 @@ stoneTransition.addEventListener("transitionend", () => {
   stonesIndex = document.querySelectorAll(".stone");
   stoneInTransition = 100;
   direction = undefined;
-});
+});*/
 
 function vinnerCheck() {}
 
